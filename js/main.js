bce816177d1895e304b7e071e9c3e0855feecadb
sokoljr <Serg607.sokol@gmail.com>
2016-08-04 00:58:22 +0300

    menu + fade_in

--- js/main.js
+++ js/main.js
@@ -1,24 +1,55 @@
 $(document).ready(function() {
+	
+	var $myNavbar = $("#fixedMenu");
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 240 && $myNavbar.hasClass("normalize")) {
+			$myNavbar.fadeOut(300,function() {
+				$(this).removeClass("normalize")
+				.addClass("fixed")
+				.fadeIn(300);
+			});
+		} else if ($(this).scrollTop() <= 240 && $myNavbar.hasClass("fixed")) {
+			$myNavbar.fadeOut(300,function()	{
+				$(this).removeClass("fixed")
+					.addClass("normalize")
+					.fadeIn(300);
+			});
+		}
+	});			
+
+
 	$('.carousel').carousel({
         interval: 3000
     });
 
-    $('#scrollup img').mouseover( function(){
-		$( this ).animate({opacity: 0.65},100);
-	}).mouseout( function(){
-		$( this ).animate({opacity: 1},100);
-	}).click( function(){
-		window.scroll(0 ,0); 
+    $('#scrollup img').mouseover( function scrollUp() {
+		$(scrollUp()).animate( {
+			opacity: 0.65
+		},100);
+	}).mouseout( function() {
+		$(scrollUp()).animate( {
+			opacity: 1
+		},100);
+	}).click( function scrollUp() {
+		$('body,html').animate( { 
+			scrollTop: 0 
+		}, 400);
 		return false;
 	});
 
-	$(window).scroll(function(){
-		if ( $(document).scrollTop() > 0 ) {
+	$(window).scroll(function() {
+		if ($(document).scrollTop() > 0) {
 			$('#scrollup').fadeIn('fast');
 		} else {
 			$('#scrollup').fadeOut('fast');
 		}
 	});
 
-
+	var $yourWeather = $('html, body');
+	$('a[href*="#your_weather"]').click(function() {
+    	$yourWeather.animate( {
+        	scrollTop: $($.attr(this, 'href')).offset().top
+    	}, 600);
+    	return false;
+	});
 });
