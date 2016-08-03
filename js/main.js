094432717672a2cdf5487e6efce6e734b64bc6b6
sokoljr <Serg607.sokol@gmail.com>
2016-08-03 20:25:02 +0300

    scroll_top

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+	$('.carousel').carousel({
+        interval: 3000
+    });
+
+    $('#scrollup img').mouseover( function(){
+		$( this ).animate({opacity: 0.65},100);
+	}).mouseout( function(){
+		$( this ).animate({opacity: 1},100);
+	}).click( function(){
+		window.scroll(0 ,0); 
+		return false;
+	});
+
+	$(window).scroll(function(){
+		if ( $(document).scrollTop() > 0 ) {
+			$('#scrollup').fadeIn('fast');
+		} else {
+			$('#scrollup').fadeOut('fast');
+		}
+	});
+
+
+});
