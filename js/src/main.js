8403528e2d20fdb56a5958b0672510aa629132a5
sokoljr <Serg607.sokol@gmail.com>
2016-08-22 11:48:52 +0300

    metrica 3.0

--- js/src/main.js
+++ js/src/main.js
@@ -46,7 +46,7 @@ $(document).ready(function() {
 	});
 
 	$(window).scroll(function() {
-		if ($(document).scrollTop() > 0) {
+		if ($(document).scrollTop() > 300) {
 			$('#scrollup').fadeIn('fast');
 		} else {
 			$('#scrollup').fadeOut('fast');
